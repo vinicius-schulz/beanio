5443777200862dded5438bb0f4f4cbe463ce5563
Vinicius Schulz <vinicius.schulz@spassu.com>
2021-04-24 15:25:33 -0300

    ajuste

--- src/main/java/com/example/flatfileparser/controller/FlatFileParserController.java
+++ src/main/java/com/example/flatfileparser/controller/FlatFileParserController.java
@@ -1,27 +1,31 @@
 package com.example.flatfileparser.controller;
 
-import com.example.flatfileparser.service.FlatFileParserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
+import com.example.flatfileparser.service.FlatFileParserService;
 
 @RestController
 @RequestMapping("fileconversion")
 public class FlatFileParserController {
 
-    private final FlatFileParserService flatFileParserService;
+	private final FlatFileParserService flatFileParserService;
 
-    @Autowired
-    public FlatFileParserController(FlatFileParserService flatFileParserService) {
-        this.flatFileParserService = flatFileParserService;
-    }
+	@Autowired
+	public FlatFileParserController(FlatFileParserService flatFileParserService) {
+		this.flatFileParserService = flatFileParserService;
+	}
 
-    @RequestMapping(method = RequestMethod.GET)
-    public void initFileLoad() throws FileNotFoundException {
-        flatFileParserService.getFile();
-    }
+	@GetMapping(value = "read")
+	public void readFile() {
+		flatFileParserService.readPositionalFile();
+	}
+
+	@GetMapping(value = "write")
+	public void writeFile() {
+		flatFileParserService.writePositionalFile();
+	}
 
 }
--- src/main/java/com/example/flatfileparser/service/FlatFileParserService.java
+++ src/main/java/com/example/flatfileparser/service/FlatFileParserService.java
@@ -2,10 +2,10 @@ package com.example.flatfileparser.service;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import org.beanio.BeanReader;
 import org.beanio.BeanReaderErrorHandlerSupport;
@@ -16,6 +16,11 @@ import org.beanio.builder.StreamBuilder;
 import org.springframework.stereotype.Service;
 
 import com.example.flatfileparser.model.GroupRoot;
+import com.example.flatfileparser.model.HeaderRecord;
+import com.example.flatfileparser.model.TX52;
+import com.example.flatfileparser.model.TX56;
+import com.example.flatfileparser.model.TX57;
+import com.example.flatfileparser.model.TX67;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -23,17 +28,82 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class FlatFileParserService {
 
-	public void getFile() throws FileNotFoundException {
+	public void readPositionalFile() {
 
 		try {
 			InputStream path = new FileInputStream(new File("input.txt"));
-			GroupRoot beanReader = createBeanReaderFromGroup(GroupRoot.class, path);
-			if (beanReader != null) {
-				writeBeanReaderFromGroup(beanReader, "output.txt");
-			}
+			GroupRoot bean = createBeanReaderFromGroup(GroupRoot.class, path);
+			log.info(bean);
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);
 		}
+	}
+
+	public void writePositionalFile() {
+
+		GroupRoot root = new GroupRoot();
+		root.setHeaderRecords(new ArrayList<>());
+		root.setTx52s(new ArrayList<>());
+		root.setTx57s(new ArrayList<>());
+
+		HeaderRecord headerRecorda = new HeaderRecord();
+		HeaderRecord headerRecordb = new HeaderRecord();
+		HeaderRecord headerRecordc = new HeaderRecord();
+
+		TX56 tx56a = new TX56();
+		TX56 tx56b = new TX56();
+		TX56 tx56c = new TX56();
+		tx56a.setNumber(866);
+		tx56b.setNumber(866);
+		tx56c.setNumber(866);
+
+		TX67 tx67a = new TX67();
+		TX67 tx67b = new TX67();
+		TX67 tx67c = new TX67();
+		tx67a.setNumber(972);
+		tx67b.setNumber(972);
+		tx67c.setNumber(972);
+
+		TX57 tx57a = new TX57();
+		TX57 tx57b = new TX57();
+		TX57 tx57c = new TX57();
+		TX57 tx57d = new TX57();
+
+		tx57a.setNumber(86659);
+		tx57b.setNumber(86659);
+		tx57c.setNumber(86659);
+		tx57d.setNumber(86659);
+
+		TX52 tx52a = new TX52();
+		TX52 tx52b = new TX52();
+		TX52 tx52c = new TX52();
+
+		tx52a.setNumber(22);
+		tx52b.setNumber(22);
+		tx52c.setNumber(22);
+
+		headerRecorda.setTx56(tx56a);
+		headerRecorda.setTx67(tx67a);
+
+		headerRecordb.setTx56(tx56b);
+		headerRecordb.setTx67(tx67b);
+
+		headerRecordc.setTx56(tx56c);
+		headerRecordc.setTx67(tx67c);
+
+		root.getHeaderRecords().add(headerRecorda);
+		root.getHeaderRecords().add(headerRecordb);
+		root.getHeaderRecords().add(headerRecordc);
+
+		root.getTx52s().add(tx52a);
+		root.getTx52s().add(tx52b);
+		root.getTx52s().add(tx52c);
+
+		root.getTx57s().add(tx57a);
+		root.getTx57s().add(tx57b);
+		root.getTx57s().add(tx57c);
+
+		writeBeanReaderFromGroup(root, "output.txt");
 
 	}
 
